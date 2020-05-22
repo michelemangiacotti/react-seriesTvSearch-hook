b66ca758cc77144478ccf7ea129ab71773b84394
bellasbra@gmail.com <bellasbra@gmail.com>
2020-05-22 22:54:01 +0200

    primo esempio hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState<string>('soprano')
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form action="">
+        <input
+          type="text"
+          placeholder = "Search TV Series"
+          value = {text}
+          onChange = {onChangeHandler}
+        />
+      </form>
     </div>
   );
 }
